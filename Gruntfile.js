8f4a14c35b2b40a2144ddd542df624571820a3a7
Artem Zolkin <arquestro93@gmail.com>
2020-06-05 12:26:11 +0300

    Messing with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,7 +166,7 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          src: ['catalyst_sphinx_theme/static/js/*.js', '!catalyst_sphinx_theme/static/js/*.min.js'],
+          src: ['catalyst_sphinx_theme/static/js/*.js', '!catalyst_sphinx_theme/static/js/*.min.js', '!catalyst_sphinx_theme/static/js/*.analytics.js'],
           dest: 'catalyst_sphinx_theme/static/js/',
           rename: function (dst, src) {
             // Use unminified file name for minified file
